842513c13741ea689ac776e6533193514e49569e
Kevin Alexander Londoño Berrio <klondono052@gmail.com>
2026-09-09 20:14:02 -0500

    Remove text color styling for specific elements
    Removed color styling for paragraph, span, label, and div elements.

--- app_nivel_cornare.py
+++ app_nivel_cornare.py
@@ -40,11 +40,6 @@ st.markdown("""
         color: #000000;
     }
 
-    /* Todo el texto */
-    p, span, label, div {
-        color: #000000;
-    }
-
     /* Títulos */
     h1, h2, h3, h4, h5, h6 {
         color: #000000;
