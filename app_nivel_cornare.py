522bcc71bc41d71402a1ce5ca6794f98cec2244a
Kevin Alexander Londoño Berrio <klondono052@gmail.com>
2026-09-09 19:56:37 -0500

    Change text color to black in metrics section

--- app_nivel_cornare.py
+++ app_nivel_cornare.py
@@ -85,6 +85,7 @@ st.markdown("""
     /* Métricas */
     [data-testid="stMetric"] {
         background-color: white;
+        color: black;
         padding: 15px;
         border-radius: 12px;
         border: 1px solid #d9e5eb;
